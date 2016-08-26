37f3b82fdf8caff0e85064ca600aa5593f4bf804
jagtapajittheneo <jagtapajit@gmail.com>
2016-08-26 13:37:56 +0530

    test
    test

---
+++ File1.js
@@ -0,0 +1,11 @@
+/**
+* Created by test.
+*/
+class testController {
+/*@ngInject*/
+  constructor($filter, $scope ,pouchDB,Restangular, $translate,$translatePartialLoader) {
+     this.test = Restangular.all('test');
+	 this.db = pouchDB('test'); 
+  }
+}
+export default testController;
